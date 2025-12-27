e90d94c1ab805f7be8b714b2973c41433dca93ac
technoboss <m.curtisdon@yahoo.co.uk>
2025-12-27 19:16:33 +0000

    user account pages added

--- app/users/routes.py
+++ app/users/routes.py
@@ -14,3 +14,27 @@ def signup():
     '''This function create a route to render the Sign up page'''
     
     return render_template('pages/signup.html',  title='Sign up')
+
+@users.route("/uaccount") 
+def uaccount():
+    '''This function create a route to render user account page''' 
+    
+    return render_template('users/useraccount.html',  title='Account')
+
+@users.route("/myprofile") 
+def myprofile():
+    '''This function create a route to render user profile page''' 
+    
+    return render_template('users/userprofile.html',  title='User Profile')
+
+@users.route("/bookings") 
+def bookings():
+    '''This function create a route to render user bookings page''' 
+    
+    return render_template('users/bookings.html',  title='Bookings')
+
+@users.route("/listings") 
+def listings():
+    '''This function create a route to render user listings page''' 
+    
+    return render_template('users/listings.html',  title='Bookings')
